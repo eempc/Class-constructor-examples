af6336995b14ffd9924d7a11e5be55e359b1bc2b
Evelyn <evc3355@gmail.com>
2019-05-24 16:25:29 +0100

    Create getset.cs

---
+++ getset.cs
@@ -0,0 +1,43 @@
+// use { get; set; } when it's a Property with PascalCase, this allows direct mutability of the property variable
+// without { get; set; } it is a (private) variable with a "method" that sets the private variable (as is the case below with the extra validation check, or gets it via return method
+
+class MyBaseClass
+{
+    // virtual auto-implemented property. Overrides can only
+    // provide specialized behavior if they implement get and set accessors.
+    public virtual string Name { get; set; }
+
+    // ordinary virtual property with backing field
+    private int num;
+    public virtual int Number
+    {
+        get { return num; }
+        set { num = value; }
+    }
+}
+
+class MyDerivedClass : MyBaseClass
+{
+    private string name;
+
+   // Override auto-implemented property with ordinary property
+   // to provide specialized accessor behavior.
+    public override string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                name = value;
+            }
+            else
+            {
+                name = "Unknown";
+            }
+        }
+    } 
+}
