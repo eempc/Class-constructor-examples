25c9a82a9e4146f1927a8afa5ee1670910b26ac9
Evelyn <evc3355@gmail.com>
2019-06-16 19:28:10 +0100

    Update casting.cs

--- casting.cs
+++ casting.cs
@@ -1,7 +1,5 @@
-public class Derived : Base
-{
-	public Derived(Base base)
-	{
+public class Derived : Base {
+	public Derived(Base base) {
 		foreach (PropertyInfo property in typeof(Base).GetProperties())
 			typeof(Derived).GetProperty(property.Name).SetValue(this, property.GetValue(base));
 	}
