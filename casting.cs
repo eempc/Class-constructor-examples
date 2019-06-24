b3a230813388d7045bde966162708b2f2c683a94
Evelyn <evc484@gmail.com>
2019-06-24 16:15:16 +0100

    Update casting.cs

--- casting.cs
+++ casting.cs
@@ -1,6 +1,7 @@
 public class Derived : Base {
 	public int NewProperty { get; set; }
-
+	
+	// Casting a base class to derived class to include the new property
 	public Derived(Base base, int newProperty) {
 		foreach (PropertyInfo property in typeof(Base).GetProperties()) {
 			typeof(Derived).GetProperty(property.Name).SetValue(this, property.GetValue(base));
@@ -8,16 +9,19 @@ public class Derived : Base {
 		NewProperty = newProperty;
 	}
 	
+	// As above but without the new property
 	public Derived(Base base) {
 		foreach (PropertyInfo property in typeof(Base).GetProperties()) {
 			typeof(Derived).GetProperty(property.Name).SetValue(this, property.GetValue(base));
 		}
 	}
 	
+	// Standard constructor inheritance for brand new instance of Derived class
 	public Derived(int newProperty, string oldProperty) : base(oldProperty) {
 		// etc
 	}
 	
+	// Just an empty
 	public Derived() {
 		
 	}
